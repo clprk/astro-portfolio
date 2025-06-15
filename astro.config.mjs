fc8f775c2ef6e4c3af1558fa8831ca885cdecffe
Ciel Park <cielcpark@gmail.com>
2025-06-15 01:54:31 -0400

    favicon, displaying posts, and failed attempt at adding /studio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import sanity from '@sanity/astro';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [
+    sanity({
+      projectId: "rnk0k1nk",
+      dataset: "production",
+      useCdn: false, // for static builds
+      // studioBasePath: '/studio' 
+    }),
+    react(),
+  ]
 });
